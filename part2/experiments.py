4e2a3009ad4dc644e2ec333f436821cc9a56409e
Chris Mailloux <mailloux.cl@gmail.com>
2018-11-30 15:04:24 -0500

    Add argparse

--- part2/experiments.py
+++ part2/experiments.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python3
 
+import argparse
+from time import time
+
 
 def main():
-    loadDataset()
+    parsedArgs = parse_command_line_arguments()
+    load_dataset(parsedArgs.path)
 
 
-def loadDataset():
-    # TODO
+def parse_command_line_arguments():
+    parser = argparse.ArgumentParser(
+        description="Classifies emails as spam or ham")
+    parser.add_argument(
+        'path',
+        help="The path to the two folders, \'test\' and \'training\'"
+    )
+    return parser.parse_args()
+
+
+def load_dataset(dataPath):
+    print(dataPath)
 
 
 if __name__ == "__main__":
