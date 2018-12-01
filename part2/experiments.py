870a358cbf5623aa14bcac55aa956a049de33b40
Chris Mailloux <mailloux.cl@gmail.com>
2018-12-01 14:07:28 -0500

    Begin work on email dataset loading

--- part2/experiments.py
+++ part2/experiments.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
+import glob
 from time import time
 
 from sklearn import svm
@@ -8,8 +10,23 @@ from sklearn.linear_model import Perceptron
 
 
 def main():
-    parsedArgs = parse_command_line_arguments()
-    load_dataset(parsedArgs.path)
+    parsed_args = parse_command_line_arguments()
+
+    data_path = parsed_args.path
+    training_emails, training_labels = load_dataset(data_path + "/training")
+    test_emails, test_labels = load_dataset(data_path + "/test")
+
+    print("Training Emails data:")
+    print(training_emails)
+    print()
+    print("Training Emails labels:")
+    print(training_labels)
+    print()
+
+    svm_classifier = train_svm_classifier(training_emails, training_labels)
+
+    results = svm_classifier.predict(test_emails)
+    print(results)
 
 
 def parse_command_line_arguments():
@@ -22,8 +39,31 @@ def parse_command_line_arguments():
     return parser.parse_args()
 
 
-def load_dataset(dataPath):
-    print(dataPath)
+def load_dataset(data_path):
+    print("Loading dataset at " + data_path)
+
+    email_data = []
+    email_labels = []
+
+    os.chdir(data_path)
+    for file in os.listdir():
+        if (file.startswith("sp")):
+            email_labels.append("spam")
+        else:
+            email_labels.append("ham")
+
+    print("Loaded " + str(len(email_labels)) + " emails")
+
+    return email_data, email_labels
+
+
+def train_svm_classifier(emails, labels):
+    classifier = svm.SVC(
+        gamma='scale',
+        C=1,
+        verbose=True)
+    classifier.fit(emails, labels)
+    return classifier
 
 
 if __name__ == "__main__":
